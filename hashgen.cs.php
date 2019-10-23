5f987d2dba800f4505c2f1a8a50555c196c27043
Jikme <29012237+Jikme@users.noreply.github.com>
2019-10-23 18:56:01 +0100

    Create hashgen.cs.php

---
+++ hashgen.cs.php
@@ -0,0 +1,33 @@
+<?php
+class HashGenerator{
+
+  public static function getmd5() {
+      $timenow  = time();
+      $bytes = random_bytes(50);
+      $hexbytes = bin2hex($bytes);
+      $secret = $timenow + $hexbytes;
+      $hashvalue = hash('md5', $secret );
+      print_r($hashvalue);
+  }
+
+    // method declaration
+    public static function getsha1() {
+        $timenow  = time();
+        $bytes = random_bytes(70);
+        $hexbytes = bin2hex($bytes);
+        $secret = $timenow + $hexbytes;
+        $hashvalue = hash('sha1', $secret );
+        print_r($hashvalue);
+    }
+
+    public static function getsha256() {
+        $timenow  = time();
+        $bytes = random_bytes(90);
+        $hexbytes = bin2hex($bytes);
+        $secret = $timenow + $hexbytes;
+        $hashvalue = hash('sha256', $secret );
+        print_r($hashvalue);
+    }
+}
+
+?>
